0ec03fb80df967286655e1567a5ec1622a947441
root <root@splunksoar>
2024-09-11 09:30:52 -0700

    Release notes for version 1.0.1

--- geocodingmapsco_consts.py
+++ geocodingmapsco_consts.py
@@ -1,6 +1,6 @@
 # File: geocodingmapsco_consts.py
 
-# Copyright (c) Unapped, 2024
+# Copyright (c) Unapproved, 2024
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
